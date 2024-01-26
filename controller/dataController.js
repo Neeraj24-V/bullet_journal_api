2b999ba4923453b09e92f076c03cce92c54e4b5c
Virodula Neeraj <virodulaneeraj@gmail.com>
2024-01-26 22:45:54 +0530

    added updateData route

--- controller/dataController.js
+++ controller/dataController.js
@@ -43,9 +43,28 @@ const getData = asyncHandler(async (req, res) => {
   });
 });
 
+// Update Data
+const updateData = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { todoId } = req.query;
+  if (!id || !todoId) {
+    res.sendStatus(400);
+    throw new Error("user_id or todoId is required");
+  }
+  const filter = {user_id: id, _id: todoId}
+  const update = req.body
+  const updatedTodo = await Data.findOneAndUpdate(filter, update)
+  if (!updatedTodo) {
+    res.sendStatus(400);
+    throw new Error("Data not updated");
+  }
+  res.status(201).json(updatedTodo)
+});
+
 // ToDo: Implement Route for querying from and to date
 
 module.exports = {
   createData,
   getData,
+  updateData
 };
--- routes/dataRoute.js
+++ routes/dataRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
-const { createData, getData } = require("../controller/dataController");
+const { createData, getData, updateData } = require("../controller/dataController");
 const validateToken = require("../middlewares/validateToken");
 
 const netRouter = express.Router();
@@ -10,6 +10,8 @@ netRouter.use(validateToken);
 
 // Route for creating Tasks
 netRouter.post("/data", createData);
+
+netRouter.patch('/data/:id', updateData)
 
 // ToDo: Implement Route for querying from and to date
 
